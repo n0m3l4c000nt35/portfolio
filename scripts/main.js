149177e349a8c71a31732fcc449b9cca764b3859
Esteban Zarate <estebanluiszarate1985@gmail.com>
2025-02-06 01:03:14 -0300

    add function to set copyright year

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,7 @@
-lucide.createIcons();
+function setYearCopyright() {
+  const currentYear = new Date().getFullYear();
+  const copyrightYearHTML = document.querySelector("#copy-year");
+  copyrightYearHTML.textContent = `${currentYear}`;
+}
+
+setYearCopyright();
