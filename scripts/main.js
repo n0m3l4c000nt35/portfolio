5aca12cbf98016db497e6ca9731a30381f42bd5d
Esteban Zarate <estebanluiszarate1985@gmail.com>
2025-02-06 21:47:46 -0300

    add function change title scroll

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,26 @@ function setYearCopyright() {
   copyrightYearHTML.textContent = `${currentYear}`;
 }
 
+function updateTitleOnScroll() {
+  const sections = document.querySelectorAll('.body__section');
+  let hasScrolled = false;
+
+  window.addEventListener("scroll", () => {
+    hasScrolled = true;
+  });
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && hasScrolled) {
+        const sectionTitle = entry.target.id.charAt(0).toUpperCase() + entry.target.id.slice(1);
+        document.title = `Portfolio - ${sectionTitle.replaceAll("-", " ")}`;
+        history.pushState(null, "", `#${entry.target.id}`);
+      }
+    });
+  }, { threshold: 0.6 });
+
+  sections.forEach(section => observer.observe(section));
+}
+
+updateTitleOnScroll();
 setYearCopyright();
